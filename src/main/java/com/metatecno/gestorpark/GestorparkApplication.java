dde6fb9f0140bbf0c63ffc35d76f1d7475013e52
Alex Cruz <alexbenicio@hotmail.com.br>
2022-04-07 16:15:29 -0300

    Estrutura projeto maven

---
+++ src/main/java/com/metatecno/gestorpark/GestorparkApplication.java
@@ -0,0 +1,13 @@
+package com.metatecno.gestorpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorparkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metatecno/gestorpark/GestorparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metatecno.gestorpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
